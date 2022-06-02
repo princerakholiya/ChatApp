5f2e708cb409b151c26df89f2ad3fe13aea948aa
princerakholiya <202051147@iiitvadodara.ac.in>
2022-06-02 16:11:12 +0530

    finaal deploy

--- client/js/client.js
+++ client/js/client.js
@@ -1,5 +1,5 @@
 
-const socket = io('https://chatappppppppp.herokuapp.com/');
+const socket = io('https://tranquil-wave-63592.herokuapp.com/');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
